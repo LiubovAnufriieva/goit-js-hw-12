6625af594fe0ffb0fbf8c5b13f735485e82b2df1
LiubovAnufriieva <lyuzika@gmail.com>
2024-04-18 16:58:45 +0300

    first refactoring hw11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,43 +1,43 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import '../css/styles.css';
+import axios from 'axios';
 
 import { createGalleryMarkup } from './render-functions';
 
-export default function onSearch(searchQuery) {
-  const KEY_API = '43230635-158e2f6795128fbec19d81d21';
-  const URL = 'https://pixabay.com/api/';
-  const searchParams = new URLSearchParams({
-    key: KEY_API,
-    q: searchQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+const KEY_API = '43230635-158e2f6795128fbec19d81d21';
+const URL = 'https://pixabay.com/api/';
 
-  fetch(`${URL}?${searchParams}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (!data.total) {
-        iziToast.error({
-          title: 'Error',
-          position: 'center',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-      createGalleryMarkup(data.hits);
-    })
-    .catch(error => {
+let page = 1;
+
+export async function onSearch(searchQuery, page = 1) {
+  try {
+    const { data } = await axios(`${URL}`, {
+      params: {
+        key: KEY_API,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 15,
+        page
+      },
+    });
+
+    if (!data.total) {
       iziToast.error({
         title: 'Error',
-        position: 'topRight',
-        message: `Oops! Something went wrong!`,
+        position: 'center',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
       });
-    })
-    .finally(() => (loader.hidden = true));
+    }
+    createGalleryMarkup(data.hits);
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      position: 'topRight',
+      message: 'Failed to fetch images. Please try again later.',
+    });
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,59 @@
 
-import onSearch from "./js/pixabay-api";
+import { onSearch } from './js/pixabay-api';
+import { createGalleryMarkup } from './js/render-functions';
 
-const form = document.querySelector(".search-form");
-const galleryList = document.querySelector(".gallery");
-const loader = document.querySelector(".loader");
+const form = document.querySelector('.search-form');
+const galleryList = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const loadBtn = document.querySelector('.load-btn');
 
-form.addEventListener("submit", onSubmit);
+form.addEventListener('submit', onSubmit);
 loader.hidden = true;
 
 function onSubmit(event) {
-    event.preventDefault();
-    galleryList.innerHTML = "";
-    loader.hidden = false;
-    const { searchRequest } = event.currentTarget.elements;
-    let searchQuery = searchRequest.value;
-    onSearch(searchQuery);
-    form.reset();
+  event.preventDefault();
+  galleryList.innerHTML = '';
+  loader.hidden = false;
+  const { searchRequest } = event.currentTarget.elements;
+  let searchQuery = searchRequest.value;
+  onSearch(searchQuery);
+  
+  form.reset();
+  
 }
 
+loadBtn.addEventListener('click', loadMore);
 
+async function loadMore() {
+  page += 1;
+  loadBtn.disabled = true;
+  
+  loader.hidden = true;
+  try {
+    const data = await onSearch(searchQuery, (page = 1));
+    galleryList.insertAdjacentHTML(
+      'beforeend',
+      createGalleryMarkup(data.results)
+    );
+    loader.hidden = true;
+
+    loadBtn.hidden = false;
+
+    const card = document.querySelector('.gallery-card');
+    const cardHeight = card.getBoundingClientRect().height;
+    window.scrollBy({
+      left: 0,
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    if (data.page >= 500) {
+      loader.hidden = true;
+    }
+  } catch (error) {
+    alert(error.message);
+  
+  } finally {
+    loader.hidden = true;
+  }
+
+}
